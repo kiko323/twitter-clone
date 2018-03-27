0223ff322cb661f07a9d041d18785905c9c659df
Christian Ciric <christian.ciric@gmail.com>
2018-03-27 15:15:54 +0200

    Code reform - variables

--- src/Entity/Posts.php
+++ src/Entity/Posts.php
@@ -19,50 +19,50 @@ class Posts {
 
 
   /**
-   * @ORM\Column(type="string", length=255, nullable=true)
+   * @ORM\Column(name="posts_email", type="string", length=255, nullable=true)
    */
-  public $posts_email;
+  public $postsEmail;
 
   /**
-   * @ORM\Column(type="string", length=255, nullable=true)
+   * @ORM\Column(name="posts_msg", type="string", length=255, nullable=true)
    */
-  public $posts_msg;
+  public $postsMsg;
 
   /**
-   * @ORM\Column(type="datetime")
+   * @ORM\Column(name="posts_created_at", type="datetime")
    */
-  public $posts_created_at;
+  public $PostsCreatedAt;
 
   public function getId () {
     return $this->id;
   }
-  
+
   public function getPostsEmail (): ?string {
-    return $this->posts_email;
+    return $this->postsEmail;
   }
 
-  public function setPostsEmail (?string $posts_email): self {
-    $this->posts_email = $posts_email;
+  public function setPostsEmail (?string $postsEmail): self {
+    $this->postsEmail = $postsEmail;
 
     return $this;
   }
 
   public function getPostsMsg (): ?string {
-    return $this->posts_msg;
+    return $this->postsMsg;
   }
 
-  public function setPostsMsg (?string $posts_msg): self {
-    $this->posts_msg = $posts_msg;
+  public function setPostsMsg (?string $postsMsg): self {
+    $this->postsMsg = $postsMsg;
 
     return $this;
   }
 
   public function getPostsCreatedAt (): ?\DateTimeInterface {
-    return $this->posts_created_at;
+    return $this->PostsCreatedAt;
   }
 
-  public function setPostsCreatedAt (\DateTimeInterface $posts_created_at): self {
-    $this->posts_created_at = $posts_created_at;
+  public function setPostsCreatedAt (\DateTimeInterface $PostsCreatedAt): self {
+    $this->PostsCreatedAt = $PostsCreatedAt;
 
     return $this;
   }
